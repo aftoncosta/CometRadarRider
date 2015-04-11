a4d885fff5aca67e64c6aeea2812d990c7c40099
Afton Straus <afton.costa@gmail.com>
2015-04-10 19:05:05 -0500

    Round trip between NodeJS and Android now works.

--- app/src/main/java/com/afton/cometradar/SetPickupSQL.java
+++ app/src/main/java/com/afton/cometradar/SetPickupSQL.java
@@ -1,18 +1,12 @@
 package com.afton.cometradar;
 
 import android.os.AsyncTask;
-
+import android.os.Build;
 import java.io.BufferedReader;
-import java.io.DataOutputStream;
 import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
 import java.net.URL;
 
-import javax.net.ssl.HttpsURLConnection;
-
-import java.net.*;
-import java.sql.*;
-import de.roderick.weberknecht.*;
 
 public class SetPickupSQL extends AsyncTask<Void, Void, String>  {
         String url = "jdbc:mysql://69.195.124.139:3306/";
@@ -34,91 +28,23 @@ public class SetPickupSQL extends AsyncTask<Void, Void, String>  {
         Background task that runs to connect to database and get route data
      */
     protected String doInBackground(Void... arg0) {
-       /* try {
 
-            System.out.println("CONNECTING TO DATABASE TO GET ROUTE DATA");
-            Class.forName(driver).newInstance();
-            Connection conn = DriverManager.getConnection(url+dbName,userName,password);
+        String temp = "INSERT INTO pickup_request (pickup_request.route_name, pickup_request.lat, pickup_request.long) VALUES ('" + MapsActivity.routeName + "', '" + lat + "', '" + lon + "');";
+        String query = temp.replace(" ", "%20");
 
-            PreparedStatement statement = (PreparedStatement) conn.prepareStatement("INSERT INTO " +
-                    "pickup_request(`route_name`,`lat`,`long`) " +
-                    "VALUES (\"" + MapsActivity.routeName + "\"," +
-                    "\""+lat+"\",\""+lon+"\")");
+        String url = "http://10.0.2.2:3001/sendPickup?string=" + query ;
 
-            System.out.println(statement);
-            statement.execute();
-            conn.close();
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-
-        */
-
-
-
-
-        /*try {
-            URI url = new URI("ws://10.0.2.2:3001");
-            WebSocket websocket = new WebSocket(url);
-
-            String temp = "INSERT INTO " +
-                    "pickup_request(`route_name`,`lat`,`long`) " +
-                    "VALUES (\"" + MapsActivity.routeName + "\"," +
-                    "\""+lat+"\",\""+lon+"\")";
-            // Register Event Handlers
-            websocket.setEventHandler(new WebSocketEventHandler() {
-                public void onOpen()
-                {
-                    System.out.println("--open");
-                }
-
-                public void onMessage(WebSocketMessage message)
-                {
-                    System.out.println("--received message: " + message.getText());
-                }
-
-                public void onClose()
-                {
-                    System.out.println("--close");
-                }
-
-                public void onPing() {}
-                public void onPong() {}
-            });
-
-            // Establish WebSocket Connections
-            websocket.connect();
-
-            // Send UTF-8 Text
-            websocket.send(temp);
-
-            // Close WebSocket Connection
-            websocket.close();
-        }
-        catch (WebSocketException wse) {
-            wse.printStackTrace();
-        }
-        catch (URISyntaxException use) {
-            use.printStackTrace();
-        }*/
-
-        String temp = "INSERT INTO " +
-                "pickup_request('route_name','lat','long') " +
-                "VALUES (\"" + MapsActivity.routeName + "\"," +
-                "\""+lat+"\",\""+lon+"\")";
-
-        String url = "http://10.0.2.2:3001/sendPickup?string=" + temp;
         try {
             URL obj = new URL(url);
             System.out.println("url swag: " + obj.toString());
             HttpURLConnection con = (HttpURLConnection) obj.openConnection();
+            if (Build.VERSION.SDK != null && Build.VERSION.SDK_INT > 13) {
+                System.out.println("CLOSE");
+                con.setRequestProperty("Connection", "close");
+            }
+            con.setRequestMethod("GET");
 
-            // optional default is GET
-            //con.setRequestMethod("GET");
-
-            //int responseCode = con.getResponseCode();
             System.out.println("\nSending 'GET' request to URL : " + url);
-            //System.out.println("Response Code : " + responseCode);
 
             BufferedReader in = new BufferedReader(
                     new InputStreamReader(con.getInputStream()));
@@ -130,11 +56,11 @@ public class SetPickupSQL extends AsyncTask<Void, Void, String>  {
             }
             in.close();
 
+
             //print result
-            System.out.println(response.toString());
+            System.out.println("RESPONSE FROM SERVER: " + response.toString());
 
         }catch(Exception e){
-            System.out.println( "error: " );
             e.printStackTrace();
         }
         return "";
